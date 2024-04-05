d4e9faf2216bd5dd8130a7a4ef7d889d106f449d
Tobias Bengfort <tobias.bengfort@posteo.de>
2024-04-05 09:00:20 +0200

    add argparse

--- xikeyring/__main__.py
+++ xikeyring/__main__.py
@@ -1,10 +1,37 @@
+import argparse
+import os
+from pathlib import Path
+
 from .dbus import DBusService
 from .dumpable import pr_set
 from .keyring import Keyring
 
+
+def get_data_home():
+    path = os.getenv('XDG_DATA_HOME')
+    if path:
+        return Path(path)
+    else:
+        return Path.home() / '.local' / 'share'
+
+
+def parse_args():
+    parser = argparse.ArgumentParser('xikeyring')
+    parser.add_argument(
+        '--store',
+        '-s',
+        help='path to the store file',
+        default=get_data_home() / 'xikeyring.db',
+    )
+    parser.add_argument(
+        '--bus', '-b', help='bus name', default='org.freedesktop.secrets'
+    )
+    return parser.parse_args()
+
+
 pr_set(dumpable=False)
 
-with Keyring('keyring.db') as keyring:
+args = parse_args()
+with Keyring(args.store) as keyring:
     service = DBusService(keyring)
-    # service.run('org.freedesktop.secrets')
-    service.run('org.ce9e.keyring')
+    service.run(args.bus)
