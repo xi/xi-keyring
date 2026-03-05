d83b0f3770fdc8a8cc55151e00bfa09fd9bd063b
Tobias Bengfort <tobias.bengfort@posteo.de>
2026-03-05 12:13:14 +0100

    app_id: use pidfd to protect against data races
    see https://github.com/swaywm/sway/pull/3088#issuecomment-456078987 for
    examples of possible attacks

--- xikeyring/app_id.py
+++ xikeyring/app_id.py
@@ -1,13 +1,21 @@
 import configparser
+import selectors
 from pathlib import Path
 
 
-def get_app_id(pid: int) -> str:
+def get_app_id(pid: int, pidfd: int) -> str:
     path = Path('/proc') / str(pid) / 'root' / '.flatpak-info'
     config = configparser.ConfigParser()
     try:
         with path.open() as fh:
             config.read_file(fh)
-        return config['Application']['name']
+        app_id = config['Application']['name']
     except Exception:
         return ''
+
+    with selectors.DefaultSelector() as sel:
+        sel.register(pidfd, selectors.EVENT_READ)
+        if sel.select() != []:
+            raise ValueError('Calling process has quit')
+
+    return app_id
