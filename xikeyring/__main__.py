b8204a6d85e93d71fa3806bdd3e6fe7f58b720f5
Tobias Bengfort <tobias.bengfort@posteo.de>
2026-03-05 20:27:17 +0100

    add --dump and --restore options to simplify migrations

--- xikeyring/__main__.py
+++ xikeyring/__main__.py
@@ -1,10 +1,12 @@
 import argparse
 import os
+import sys
 from pathlib import Path
 
 from .dbus import DBusService
 from .dumpable import pr_set
 from .keyring import KeyringProxy
+from .keyring import write_bytes
 
 
 def get_data_home():
@@ -17,6 +19,16 @@ def get_data_home():
 
 def parse_args():
     parser = argparse.ArgumentParser('xikeyring')
+    parser.add_argument(
+        '--dump',
+        help='print the decryted file and exit',
+        action='store_true',
+    )
+    parser.add_argument(
+        '--restore',
+        help='inverse of --dump',
+        action='store_true',
+    )
     parser.add_argument(
         '--store',
         '-s',
@@ -33,5 +45,15 @@ pr_set(dumpable=False)
 
 args = parse_args()
 keyring = KeyringProxy(args.store)
-service = DBusService(keyring)
-service.run(args.bus)
+if args.dump:
+    with open(keyring.path, 'rb') as fh:
+        encrypted = fh.read()
+    decrypted = keyring.crypt.decrypt(encrypted)
+    print(decrypted.decode('utf-8'))
+elif args.restore:
+    decrypted = sys.stdin.read().encode('utf-8')
+    encrypted = keyring.crypt.encrypt(decrypted)
+    write_bytes(keyring.path, encrypted)
+else:
+    service = DBusService(keyring)
+    service.run(args.bus)
